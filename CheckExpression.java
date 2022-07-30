08f25d14cf32304786cbea4deaa18ca7a4cd4925
Breiner Batalla <batallabreiner69@gmail.com>
2022-07-29 23:06:49 -0500

    Stack exercise, check if the expression is correct.

---
+++ CheckExpression.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+public class CheckExpression {
+
+    public static void main(String[] args) {
+
+        System.out.println(getExp("[(})"));
+        System.out.println(getExp("[(({}))]"));
+
+    }
+
+    static boolean getExp(String exp) {
+        Stack<Character> stack= new Stack<>();
+        boolean correct=true;
+
+        for(int i=0; i< exp.length(); i++) {
+            char temp = exp.charAt(i);
+
+            if(temp=='{'){
+                stack.push('}');
+            }else if(temp=='['){
+                stack.push(']');
+            }
+            else if(temp=='('){
+                stack.push(')');
+            }
+            else if(temp!=stack.pop()){
+                correct=false;
+            }
+        }
+
+        return correct;
+    }
+}
